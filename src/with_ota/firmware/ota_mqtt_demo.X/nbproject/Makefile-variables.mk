15f76ab3d49f9cc44b7bcfa612ac7998e7f76060
Pranjal Das <Pranjal.Das@microchip.com>
2021-07-29 16:39:53 +0530

    Folder structure change

---
+++ src/with_ota/firmware/ota_mqtt_demo.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mzw1 configuration
+CND_ARTIFACT_DIR_pic32mzw1=dist/pic32mzw1/production
+CND_ARTIFACT_NAME_pic32mzw1=ota_mqtt_demo.X.production.hex
+CND_ARTIFACT_PATH_pic32mzw1=dist/pic32mzw1/production/ota_mqtt_demo.X.production.hex
+CND_PACKAGE_DIR_pic32mzw1=${CND_DISTDIR}/pic32mzw1/package
+CND_PACKAGE_NAME_pic32mzw1=otamqttdemo.x.tar
+CND_PACKAGE_PATH_pic32mzw1=${CND_DISTDIR}/pic32mzw1/package/otamqttdemo.x.tar
